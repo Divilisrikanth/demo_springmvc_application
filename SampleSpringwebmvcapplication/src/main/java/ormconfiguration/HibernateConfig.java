a41dd01a0d4efc6008dbb0a2806fae41360d6c43
DiviliSrikanth <srikanthdivili1994@gmail.com>
2025-10-10 19:24:50 +0530

    addedhibcnfg

--- SampleSpringwebmvcapplication/src/main/java/ormconfiguration/HibernateConfig.java
+++ SampleSpringwebmvcapplication/src/main/java/ormconfiguration/HibernateConfig.java
@@ -1,11 +1,11 @@
 package ormconfiguration;
 
 import java.util.Properties;
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -16,27 +16,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:application.properties")
-
+@PropertySource("classpath:db.properties")
+@ComponentScan(basePackages = "ormconfiguration")
 public class HibernateConfig {
 
     @Autowired
     private Environment environment;
-    
-    @PostConstruct
-    public void checkResource() {
-        System.out.println("Resource exists? " + getClass().getClassLoader().getResource("application.properties"));
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("ormconfiguration.entities");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        System.out.println("inside seesion factory");
-        return sessionFactory;
-    }
 
     @Bean
     public DataSource dataSource() {
@@ -45,23 +30,29 @@ public class HibernateConfig {
         dataSource.setUrl(environment.getRequiredProperty("db.url"));
         dataSource.setUsername(environment.getRequiredProperty("db.username"));
         dataSource.setPassword(environment.getRequiredProperty("db.password"));
-        
-
         return dataSource;
     }
 
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        return properties;
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ormconfiguration.entities");
+
+        Properties props = new Properties();
+        props.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        props.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        sessionFactory.setHibernateProperties(props);
+
+        System.out.println("✅ SessionFactory initialized");
+        return sessionFactory;
     }
 
     @Bean
-    public HibernateTransactionManager getTransactionManager() {
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
+        transactionManager.setSessionFactory(sessionFactory);
         return transactionManager;
     }
 }
